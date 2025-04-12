0418dbc6bf1b80b0a7c8cca557b5bbba97f6e507
Ian <105128049+minijae011030@users.noreply.github.com>
2025-04-12 21:20:32 +0900

    Feat: delete todo 기능 추가

--- src/Pages/TodoList/DetailPage.vue
+++ src/Pages/TodoList/DetailPage.vue
@@ -4,7 +4,6 @@
     <h1>detail page</h1>
     <h2>{{ todo.todo }}</h2>
     <p>{{ todo.desc }}</p>
-    <input type="checkbox" disabled v-bind:checked="todo.done === true" />
 
     <button @click="handleEditButtonClick">수정하기</button>
     <button @click="handleDeleteButtonClick">삭제하기</button>
@@ -30,7 +29,13 @@ const handleEditButtonClick = () => {
   router.push(`/edit/${id}`)
 }
 
-const handleDeleteButtonClick = () => {}
+const handleDeleteButtonClick = async () => {
+  if (window.confirm('삭제하시겠습니까?')) {
+    const result = await axios.delete(`http://localhost:3000/todos/${id}`)
+    console.log('삭제되었습니다.')
+    router.push('/todo')
+  }
+}
 
 const fetchTodoDetail = async () => {
   const result = await axios.get(`http://localhost:3000/todos/${id}`)
