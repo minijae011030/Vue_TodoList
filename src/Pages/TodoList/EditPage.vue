415e88ac167f5dc8c6fe87a579f1bcef443a1367
Ian <105128049+minijae011030@users.noreply.github.com>
2025-04-13 16:35:53 +0900

    Design: EditPage 디자인

--- src/Pages/TodoList/EditPage.vue
+++ src/Pages/TodoList/EditPage.vue
@@ -1,11 +1,45 @@
 <template lang="">
-  <div>
-    <h1>edit page</h1>
-    <p>todo title: <input type="text" v-model.trim="todo.todo" /></p>
-    <p>todo desc: <input type="text" v-model.trim="todo.desc" /></p>
-    <input type="checkbox" v-model="todo.done" />
-    <button @click="handleEditButtonClick">수정 완료</button>
-    <button @click="goBack">취소</button>
+  <div class="flex flex-col gap-4 w-[500px] m-auto items-center">
+    <div class="w-full items-start">
+      <button @click="goBack" class="flex gap-2 text-gray-500">
+        <i class="bi bi-arrow-left"></i>
+        뒤로가기
+      </button>
+    </div>
+    <h1 class="text-xl font-bold">Todo 편집 페이지</h1>
+
+    <form class="flex flex-col gap-5 border rounded-lg p-10">
+      <div class="flex gap-3 text-center items-center">
+        <p class="w-[80px] text-left">제목</p>
+        <input
+          class="border rounded-md px-4 py-2 w-[300px]"
+          type="text"
+          placeholder="todo 제목을 입력하세요"
+          v-model.trim="todo.todo"
+        />
+      </div>
+      <div class="flex gap-3 text-center items-center">
+        <p class="w-[80px] text-left">제목</p>
+        <input
+          class="border rounded-md px-4 py-2 w-[300px]"
+          type="text"
+          placeholder="todo 제목을 입력하세요"
+          v-model.trim="todo.desc"
+        />
+      </div>
+
+      <div class="w-full text-right px-3 flex justify-end gap-3">
+        <p>완료</p>
+        <input type="checkbox" v-model="todo.done" />
+      </div>
+      <button
+        class="border rounded-full p-2 w-full mt-6"
+        type="submit"
+        @click="handleEditButtonClick"
+      >
+        수정 완료
+      </button>
+    </form>
   </div>
 </template>
 <script setup>
