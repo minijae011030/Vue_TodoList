ebc7e7983ca8c2e7940a1445b22d5f296ad37e78
Ian <105128049+minijae011030@users.noreply.github.com>
2025-04-13 15:53:34 +0900

    Design: TodoPage 디자인

--- src/Pages/TodoList/TodoPage.vue
+++ src/Pages/TodoList/TodoPage.vue
@@ -1,11 +1,20 @@
 <template lang="">
-  <div>
-    <h1>Todo 조회하기</h1>
+  <div class="flex flex-col gap-10 items-center w-[500px] m-auto">
+    <h1 class="text-xl font-bold">{{ auth.user.name }}님의 TodoList</h1>
 
-    <li v-for="(item, index) in todoArr">
-      <input type="checkbox" disabled :checked="item.done === true" />
-      <RouterLink :to="`/detail/${item.id}`">{{ item.todo }}</RouterLink>
-    </li>
+    <div class="w-full flex flex-col gap-3">
+      <RouterLink :to="`/detail/${item.id}`" v-for="(item, index) in todoArr">
+        <li
+          class="list-none flex justify-between w-full px-4 py-3 border rounded-lg hover:bg-gray-100 transition-all"
+        >
+          <div class="flex gap-2">
+            <input type="checkbox" disabled :checked="item.done === true" />
+            <p>{{ item.todo }}</p>
+          </div>
+          <i class="bi bi-arrow-right-circle-fill"></i>
+        </li>
+      </RouterLink>
+    </div>
   </div>
 </template>
 <script setup>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createPinia } from 'pinia'
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { createApp } from 'vue'
 
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import App from './App.vue'
 import './assets/tailwind.css'
 import router from './router'
