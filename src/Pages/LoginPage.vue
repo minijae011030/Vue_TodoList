6b6ad359f8bbfe1d106d4a33ece0f609a84e1065
Ian <105128049+minijae011030@users.noreply.github.com>
2025-04-13 16:00:53 +0900

    Design: 로그인 페이지 디자인

--- src/Pages/LoginPage.vue
+++ src/Pages/LoginPage.vue
@@ -1,14 +1,33 @@
 <template lang="">
-  <div>
-    <h1>Login</h1>
-    <form>
-      <p>이메일: <input v-model.trim="email" type="text" placeholder="email을 입력하세요" /></p>
-      <p>
-        비밀번호:
-        <input v-model.trim="password" type="password" placeholder="비밀번호를 입력하세요" />
-      </p>
+  <div class="flex flex-col items-center gap-10">
+    <h1 class="text-xl font-bold">Login</h1>
+    <form class="flex flex-col gap-5">
+      <div class="flex gap-3 text-center items-center">
+        <p class="w-[100px]">이메일</p>
+        <input
+          class="border rounded-md px-4 py-2 w-[300px]"
+          v-model.trim="email"
+          type="text"
+          placeholder="email을 입력하세요"
+        />
+      </div>
+      <div class="flex gap-3 text-center items-center">
+        <p class="w-[100px]">비밀번호</p>
+        <input
+          class="border rounded-md px-4 py-2 w-[300px]"
+          v-model.trim="password"
+          type="password"
+          placeholder="비밀번호를 입력하세요"
+        />
+      </div>
 
-      <button type="submit" @click="login">로그인</button>
+      <button
+        class="mt-6 border rounded-full p-3 hover:bg-gray-100 transition-all"
+        type="submit"
+        @click="login"
+      >
+        로그인
+      </button>
     </form>
   </div>
 </template>
